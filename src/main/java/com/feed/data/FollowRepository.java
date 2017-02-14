efa914b2e0620260a0602c8185cb111e3bbeec40
JungChanHyeong <chanh327@gmail.com>
2017-02-14 14:44:58 +0900

    modify

--- src/main/java/com/feed/data/FollowRepository.java
+++ src/main/java/com/feed/data/FollowRepository.java
@@ -4,12 +4,7 @@ import com.feed.model.Follow;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface FollowRepository extends JpaRepository<Follow, Long> {
 
-    List<Follow> findAllByFollower(String follower);
-
-    void removeByFollower(String follower);
 }
